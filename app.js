70e1a26db6c54b26c82227714b9156b5cd2e6e85
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-12 13:10:13 +0200

    remove intentional bug

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT + 3, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT + 3}`)
+  console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (req, res) => {
